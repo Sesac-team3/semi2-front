843a77d5e98d13f5c11dd54005e2e34dd0d4d1ec
Hyungs0703 <hyungs0703@naver.com>
2024-12-20 17:21:16 +0900

    제목: 메인페이지 구현중

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
+            <Route path="/home" element={<Home />} />
           </Routes>
         </div>
       </Router>
